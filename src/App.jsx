429835521112d0dd61bd616b10ff93796eca5189
Boris Kudryavtsev <boriskk.work@gmail.com>
2020-05-18 13:29:35 -0700

    Protect and regenerate API keys

--- src/App.jsx
+++ src/App.jsx
@@ -95,7 +95,7 @@ const App = () => {
     mapsApiScript.type = 'text/javascript';
     mapsApiScript.defer = true;
     mapsApiScript.async = true; 
-    mapsApiScript.src = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyCUInr7_6lymolmDGssyJGl4BuIWjV_0oQ&callback=initMaps'; 
+    mapsApiScript.src = `https://maps.googleapis.com/maps/api/js?key=${process.env.GOOGLE_MAPS_API_KEY}&callback=initMaps`; 
 
     document.body.appendChild(mapsApiScript);
   }, []);
