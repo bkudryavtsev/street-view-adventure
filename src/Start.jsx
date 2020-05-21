3a4a2eced98ce689a166fe55c0c9543f12ab6871
Boris Kudryavtsev <boriskk.work@gmail.com>
2020-05-21 00:34:05 -0700

    Fix play mode selection and adjust colors

--- src/Start.jsx
+++ src/Start.jsx
@@ -14,18 +14,23 @@ const Start = props => {
   const { colors } = themeContext;
 
   const [mode, setMode] = useState({
-    multiplayer: false,
+    playMode: 0,
     duration: 5
   });
 
   const sessionId = Math.random().toString(36).replace(/[^a-z]+/g, '').substr(0, 10);
 
+  const playModes = [
+    [0, { title: 'Endure alone', multiplayer: false, icon: SingleplayerImage }], 
+    [1, { title: 'Explore with friends', multiplayer: true, icon: MultiplayerImage }]
+  ];
+  const playModeMap = new Map(playModes);
+
   const durations = [['Quick', 5], ['Tourist', 10], ['Explorer', 'Untimed']];
   const durationMap = new Map(durations);
 
-  const onPlayModeSelect = (event, multiplayer) => {
-    event.currentTarget.classList.add('selected');
-    setMode({ ...mode, multiplayer })
+  const onPlayModeSelect = playMode => {
+    setMode({ ...mode, playMode });
   };
 
   const onDurationSelect = duration => {
@@ -45,25 +50,24 @@ const Start = props => {
             </div>
             <div className="options">
               <div className="column">
-                <div 
-                  onClick={event => onPlayModeSelect(event, false)}
-                  className="mode-button">
-                  <img src={SingleplayerImage} />
-                  <h3>Endure alone</h3>
-                </div>
-                <div 
-                  onClick={event => onPlayModeSelect(event, true)} 
-                  className="mode-button">
-                  <img src={MultiplayerImage} />
-                  <h3>Explore with friends</h3>
-                </div>
+                {Array.from(playModeMap.keys()).map(playMode => {
+                  const playModeDetails = playModeMap.get(playMode);
+                  return(
+                    <div 
+                      className={`mode-button ${playMode === mode.playMode ? 'selected' : ''}`} 
+                      onClick={event => onPlayModeSelect(playMode)}>
+                      <img src={playModeDetails.icon} />
+                      <h3>{playModeDetails.title}</h3>
+                    </div>
+                  );
+                })}
               </div>
               <div className="row">
                 {Array.from(durationMap.keys()).map(durationName => {
                   const duration = durationMap.get(durationName);
                   return(
                     <div 
-                      className="mode-button small" 
+                      className={`mode-button small ${duration === mode.duration ? 'selected' : ''}`}
                       onClick={event => onDurationSelect(duration)}>
                       <h3>{durationName}</h3>
                       <p>{duration} {!isNaN(duration) && 'min'}</p>
