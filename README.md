c803f2d2ba4bdbb09803d686fdcbfdeaefac0232
Boris Kudryavtsev <boriskk.work@gmail.com>
2020-06-06 11:31:42 -0700

    Update RemoteStats styling and readme

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 # Street View Adventure
 A web-based geography guessing game inspired by [GeoGuessr](https://www.geoguessr.com/) with account-less multiplayer and (virtually) unlimited locations. Players are placed in a semi-random Street View location and are required to guess as many locations as they can by looking for clues in the interactive panorama before the time runs out.
 
-![Image of lobby](https://i.ibb.co/t3pCbsF/Screen-Shot-2020-05-31-at-14-19-57.png)
+![Image of lobby](https://i.ibb.co/WHdbNcW/Screen-Shot-2020-06-06-at-11-24-22.png)
+![Image of game](https://i.ibb.co/d4Yhf1Q/Screen-Shot-2020-06-06-at-11-28-20.png)
 
 ## Stack
 Street View Adventure utilizes the Google Maps JavaScript API and React.js bundled with Parcel and Sass. Firebase is used as a backend for data and session storage.
--- src/RemoteStats.jsx
+++ src/RemoteStats.jsx
@@ -1,23 +1,47 @@
 import React, { useContext, useEffect, useState } from 'react';
 import { AppContext } from './AppContext';
-import { getSessionUsers } from './util/db';
+import { onUsersChange } from './util/db';
 
 const RemoteStats = props => {
   const [appContext, appDispatch] = useContext(AppContext);
 
   const [users, setUsers] = useState([]);
+  const [usersSnapshot, setUsersSnapshot] = useState(null);
 
   const { sessionParams, userParams } = appContext;
   const { sessionId } = sessionParams;
 
   useEffect(() => {
     if (sessionId) {
-      getSessionUsers(sessionId).then(docs => {
-        const usersData = docs.map(userDoc => userDoc.data());
-        setUsers(usersData);
-      });
+      const releaseUsersListener = onUsersChange(sessionId, snapshot => 
+        setUsersSnapshot(snapshot));
+
+      return () => {
+        releaseUsersListener();
+      };
     }
   }, [sessionId]);
+
+  useEffect(() => {
+    if (usersSnapshot) {
+      let updatedUsers = [...users];
+      usersSnapshot.docChanges().forEach(change => {
+        switch(change.type) {
+          case 'added':
+            updatedUsers.push(change.doc.data());
+            break;
+          case 'modified':
+            const modifiedUser = change.doc.data();
+            const idx = users.findIndex(user => user.id === modifiedUser.id);
+            updatedUsers[idx] = modifiedUser;
+            break;
+          default:
+        }
+      });
+
+      setUsers(updatedUsers);
+    }
+  }, [usersSnapshot]);
 
   return(
     <div className="info-bar-stats">
@@ -25,7 +49,7 @@ const RemoteStats = props => {
         return(
           <div className="user" key={user.id}>
             <img src={user.avatar} />
-            <p>{user.name}</p>
+            <p>{user.score > 9999 ? '9999+' : user.score}</p>
           </div>
         );
       })}
